be3efccab56e632e55c12cbd4ab8dbe79528fe93
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-05-13 11:14:31 +0200

    pluralsight course

---
+++ Grades/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(78.5f);
+            book.AddGrade(59);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            book = new GradeBook();
+            book.AddGrade(75);
+
+        }
+    }
+}
